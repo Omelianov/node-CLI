11ed9a5447a72cb924322f68c94219d50a41cbd6
Omelianov Nicolai <omelianov.n@gmail.com>
2023-07-17 23:51:29 +0300

    added starting files

---
+++ index.js
@@ -0,0 +1,21 @@
+// import { listContacts } from './contacts.js';
+
+const contactsItems = require('./contacts')
+
+contactsItems.readFile('readFile function');
+contactsItems.listContacts('listContacts function');
+
+
+const nodemon = require("nodemon");
+
+// console.log('Hello world')
+
+
+
+// const date = new Date();
+
+// console.log(date.getMilliseconds())
+
+
+
+
