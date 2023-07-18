e78143f692665b3da738646fcaa3566da2ca53a2
Omelianov Nicolai <omelianov.n@gmail.com>
2023-07-18 03:04:14 +0300

    changed structure of project

--- contacts.js
+++ contacts.js
@@ -1,112 +1,78 @@
 const fs = require('fs/promises');
 const path = require('path');
-
-// TODO: задокументировать каждую функцию
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
+
 async function readFile() {
-    try {
-        const data = await fs.readFile(contactsPath, 'utf-8');
-        return JSON.parse(data);
-    } catch (error) {
-        console.error("Error reading file ${error}");
-        return null;
-    }
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    return JSON.parse(data);
 }
 
 async function writeFile(contacts) {
-    try {
-        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-        console.log('Contacts saved successfully.');
-    } catch (error) {
-        console.error(`Error writing file: ${error}`);
-    }
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log('Contacts saved successfully.');
 }
 
-// Возвращает массив контактов.
+// Возвращает массив контактов
 
-// async function listContacts() {
-//     try {
-//         const contacts = await readFile();
-//         if (contacts) {
-//             console.table(contacts);
-//         } else {
-//             console.log('No contacts found.');
-//         }
-//     } catch (error) {
-//         console.error(`Error listing contacts: ${error}`);
-//     }
-// }
+async function listContacts() {
+    const contacts = await readFile();
+    if (contacts) {
+        console.table(contacts);
+    } else {
+        console.log('No contacts found.');
+    }
+}
 
 
 // Возвращает объект контакта с таким id.Возвращает null, если объект с таким id не найден.
 
-// async function getContactById(contactId) {
-//     try {
-//         const contacts = await readFile();
-//         if (contacts) {
-//             const contact = contacts.find((c) => c.id === contactId);
-//             return contact || null;
-//         } else {
-//             return null;
-//         }
-//     } catch (error) {
-//         console.error("Error getting contact by ID: ${error}");
-//         return null;
-
-//     }
-
-// }
+async function getContactById(id) {
+    const contacts = await readFile();
+    if (contacts) {
+        const contact = contacts.find((c) => c.id === id);
+        return contact || null;
+    } else {
+        return null;
+    }
+}
 
 
 
 // Возвращает объект удаленного контакта.Возвращает null, если объект с таким id не найден.
 
-// async function removeContact(contactId) {
-//     try {
-//         const contacts = await readFile();
-//         if (contacts) {
-//             const updatedContacts = contacts.filter((c) => c.id !== contactId);
-//             await writeFile(updatedContacts);
-//             return updatedContacts.length !== contacts.length;
-//         } else {
-//             return false;
-//         }
-//     } catch (error) {
-//         console.error(`Error removing contact: ${error}`);
-//         return false;
-//     }
-// }
-
+async function removeContact(id) {
+    const contacts = await readFile();
+    if (contacts) {
+        const updatedContacts = contacts.filter((c) => c.id !== id);
+        await writeFile(updatedContacts);
+        return updatedContacts.length !== contacts.length;
+    } else {
+        return false;
+    }
+}
 
 
 // Возвращает объект добавленного контакта. 
 
-// async function addContact(name, email, phone) {
-//     try {
-//         const contacts = await readFile();
-//         const newContact = {
-//             id: Date.now(),
-//             name,
-//             email,
-//             phone,
-//         };
-//         contacts.push(newContact);
-//         await writeFile(contacts);
-//         console.table(newContact)
-//         return newContact;
-//     } catch (error) {
-//         console.error(`Error adding contact: ${error}`);
-//         return null;
-//     }
-// }
+async function addContact(data) {
+    const contacts = await readFile();
+    const newContact = {
+        id: nanoid(),
+        ...data
+    };
+    contacts.push(newContact);
+    await writeFile(contacts);
+    return newContact;
+}
 
 module.exports = {
-    // readFile,
-    // listContacts,
-    // getContactById,
-    // removeContact,
-    // addContact
-
+    readFile,
+    writeFile,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
 }
--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-// import { listContacts } from './contacts.js';
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 const nodemon = require("nodemon");
 
-const contactsItems = require('./contacts')
-
-// contactsItems.readFile('readFile function');
-// contactsItems.listContacts('listContacts function');
-// contactsItems.getContactById('getContactById function');
-// contactsItems.removeContact('removeContact function');
-// contactsItems.addContact('addContact function');
+const contacts = require("/contacts");
 
 
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case 'list':
+            const allContacts = await contacts.listContacts();
+            console.log(allContacts);
+            break;
+
+        case 'get':
+            const getByID = await contacts.getContactById(id);
+            console.log(getByID);
+            break;
+
+        case 'add':
+            const addNewContact = await contacts.addContact(name, email, phone);
+            console.log(addNewContact);
+            break;
+
+        case 'remove':
+            const removeByID = await contacts.removeContact(id);
+            console.log(removeByID);
+            break;
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
 
 
 
